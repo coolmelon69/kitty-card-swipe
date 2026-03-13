a9a7e5e3453163eabfa43fdcd5b54e89811b0a85
adam.danial <adam.danial@uniten.edu.my>
2026-03-13 14:58:28 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/kitty-card-swipe/",
   server: {
     host: "::",
     port: 8080,
